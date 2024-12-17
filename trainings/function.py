30f8ee997750172db2dfc56d23e25905ecc5ff43
starsden <159563356+starsden@users.noreply.github.com>
2024-12-17 18:05:54 +0300

    add trainings

---
+++ trainings/function.py
@@ -0,0 +1,15 @@
+"""
+def loo():
+    print("hello")
+
+def loo2(name, age):
+    print("hello", name)
+    print("Возраст:", age, "лет")
+
+loo2(name = input("введи имя "), age = int(input("сколько лет? ")))
+"""
+
+def loo3(a, b):
+    return a + b
+
+print("Сумма чисел: ", loo3(a = int(input("1 число: ")), b = int(input("второе число: "))))
