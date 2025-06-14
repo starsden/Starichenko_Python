96f58b19faa5d2ba71d96212f712a6a5552069f2
starsden <159563356+starsden@users.noreply.github.com>
2025-06-14 11:52:05 +0300

    add exam 12/13

--- exam.py
+++ exam.py
@@ -1,21 +1,22 @@
-import sqlite3 as sq
+'''
+В двумерном списке найти сумму и произведение элементов строки N (N задать с клавиатуры).
+'''
 
-with sq.connect('clients.db') as con:
-    cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS clients
-                (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                fio TEXT NOT NULL,
-                percen INT NOT NULL,
-                srok INT NOT NULL);""")
+import random
 
-    data = [
-        ['Стариченко Денис Евгеньевич', '5', '100'],
-        ['Шакулин Аристарх Андреевич', '4', '200'],
-        ['Чубенко Алина Сергеевна', '3', '300']
-    ]
-    cur.executemany("INSERT INTO clients (fio, percen, srok) VALUES (?, ?, ?)",data)
-    con.commit()
+cols = 3
+rows = 3
+matrix = [[random.randint(1, 10) for _ in range(cols)] for _ in range(rows)]
+print(matrix)
+def mult(a):
+    N = int(input("введите номер строки: "))
+    if 0 <= N < 4:
+        norm = N - 1
+        p = 1
+        for i in range(len(matrix[norm])):
+            p *= matrix[norm][i]
+        print(f"произведение: {p}")
+    else:
+        return None
 
-a = int(input('введите id '))
-cur.execute("DELETE FROM clients WHERE id = ?", (a,))
-con.commit()
+mult(matrix)
